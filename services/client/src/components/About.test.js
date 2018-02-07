437861a5951445177feab50c676b7abbc08bf6fd
CeX <tundaey@gmail.com>
2018-02-07 00:33:31 +0000

    fixing my stupiggit issues

--- services/client/src/components/About.test.js
+++ services/client/src/components/About.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 
 import About from './About'
 
-test('About render properly', ()=> {
+test('About renders properly', ()=> {
     const wrapper = shallow(<About/>)
     const element = wrapper.find('p')
     expect(element.length).toBe(1)
